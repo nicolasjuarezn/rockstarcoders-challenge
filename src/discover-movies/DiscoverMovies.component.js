c59057c122bd249034affcd492f0d9eb526ef0ec
Nicolas Juarez N <nicolas.juarez@gm2dev.com>
2020-10-22 19:34:35 -0300

    feat(discover-movies): initial module component

---
+++ src/discover-movies/DiscoverMovies.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function DiscoverMovies() {
+  return (
+    <ul>
+      <li>
+        <a href="#1">Movie A</a>
+      </li>
+      <li>
+        <a href="#1">Movie B</a>
+      </li>
+    </ul>
+  );
+}
