e32f0983c71f0c5c790b77d90bb0a6e093990cd9
Nicolas Juarez N <nicolas.juarez@gm2dev.com>
2020-10-22 20:11:29 -0300

    feat(movies.service.js): implement getDiscoverMovies

---
+++ src/services/movies.service.js
@@ -0,0 +1,32 @@
+class MovieService {
+  constructor() {
+    this.APIKey = "0650dc23d9158fa968f70eaad71ff3fd";
+    this.baseURL = "https://api.themoviedb.org/3/";
+  }
+
+  buildURL({ path, query }) {
+    return `${this.baseURL}${path}?api_key=${this.APIKey}${query}`;
+  }
+
+  async fetchProvider() {
+    const response = await fetch(...arguments);
+    return await response.json();
+  }
+
+  async getDiscoverMovies() {
+    try {
+      const discoverMoviesURL = this.buildURL({
+        path: "discover/movie",
+        query: "&sort_by=popularity.desc&page=1",
+      });
+
+      const { results } = await this.fetchProvider(discoverMoviesURL);
+
+      return results;
+    } catch (error) {
+      console.warn("discoverMovies", error);
+    }
+  }
+}
+
+export const movieService = new MovieService();
