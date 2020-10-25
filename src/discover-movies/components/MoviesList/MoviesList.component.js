78a0be055a9c36e7c93376b81fc2d057e65a6684
Nicolas Juarez N <nicolas.juarez@gm2dev.com>
2020-10-25 20:50:16 -0300

    feat(MoviesList.component.js): initial styles

--- src/discover-movies/components/MoviesList/MoviesList.component.js
+++ src/discover-movies/components/MoviesList/MoviesList.component.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./MoviesList.module.css";
 
 export function MoviesList({ movies, errorMessageResult }) {
   return (
     errorMessageResult || (
-      <ul>
+      <ul className={styles.movie_list}>
         {movies.map(({ title, id, vote_average, poster_path }) => (
-          <li key={id}>
+          <li key={id} className={styles.movie_list__li}>
             <Link to={`movie/${id}`}>
               {title} - {vote_average}
               <img src={poster_path} alt={title} />
