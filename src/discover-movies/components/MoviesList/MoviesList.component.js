1409761a38aeac994df70917e8ada52a60794069
Nicolas Juarez N <nicolas.juarez@gm2dev.com>
2020-10-25 20:34:32 -0300

    feat(MoviesList.component.js): implement poster

--- src/discover-movies/components/MoviesList/MoviesList.component.js
+++ src/discover-movies/components/MoviesList/MoviesList.component.js
@@ -6,10 +6,11 @@ export function MoviesList({ movies, errorMessageResult }) {
   return (
     errorMessageResult || (
       <ul>
-        {movies.map(({ title, id, vote_average }) => (
+        {movies.map(({ title, id, vote_average, poster_path }) => (
           <li key={id}>
             <Link to={`movie/${id}`}>
               {title} - {vote_average}
+              <img src={poster_path} alt={title} />
             </Link>
           </li>
         ))}
