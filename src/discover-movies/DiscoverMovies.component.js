eea3391f7ba9f875dcd096e90a3337acc19f7f7e
Nicolas Juarez N <nicolas.juarez@gm2dev.com>
2020-10-25 17:01:35 -0300

    feat(DiscoverMovies.component.js): minimum application of RateFilterMovies.component.js

--- src/discover-movies/DiscoverMovies.component.js
+++ src/discover-movies/DiscoverMovies.component.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { moviesService } from "../services/movies.service";
+import { RateFilterMovies } from "./components/RateFilterMovies/RateFilterMovies.component";
 import { SearchMovies } from "./components/SearchMovies/SearchMovies.component";
 
 export function DiscoverMovies() {
   const [movies, setMovies] = useState([]);
   const [movieSearchResult, setMovieSearchResults] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
 
   useEffect(
     function getMovies() {
@@ -15,13 +17,25 @@ export function DiscoverMovies() {
 
   const moviesList = movieSearchResult.length ? movieSearchResult : movies;
 
+  const onFilterChange = ({ minRange, maxRange }) => {
+    const filteredMovies = moviesList.filter(
+      ({ vote_average }) => vote_average >= minRange && vote_average <= maxRange
+    );
+    setFilteredMovies(filteredMovies);
+  };
+
+  const moviesToRender = filteredMovies.length ? filteredMovies : moviesList;
+
   return (
     <div>
       <SearchMovies onResults={setMovieSearchResults} />
+      <RateFilterMovies onFilterChange={onFilterChange} />
       <ul>
-        {moviesList.map(({ title, id }) => (
+        {moviesToRender.map(({ title, id, vote_average }) => (
           <li key={id}>
-            <a href="#1">{title}</a>
+            <a href="#1">
+              {title} - {vote_average}
+            </a>
           </li>
         ))}
       </ul>
