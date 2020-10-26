6317980b4cd733e77e33bd7464d707efafdaf04c
Nicolas Juarez N <nicolas.juarez@gm2dev.com>
2020-10-26 16:50:39 -0300

    feat(SearchMovies.component.js): implement defaultValue

--- src/discover-movies/components/SearchMovies/SearchMovies.component.js
+++ src/discover-movies/components/SearchMovies/SearchMovies.component.js
@@ -5,6 +5,7 @@ import {
   search_form__input,
   search_form__button,
 } from "./SearchMovies.module.css";
+import { parseQuery } from "../../../core/helpers/query-parse.helpers";
 
 export function SearchMovies({ onSubmit }) {
   const searchInput = useRef(null);
@@ -12,14 +13,24 @@ export function SearchMovies({ onSubmit }) {
   const onSubmitSearch = (event) => {
     event.preventDefault();
     const searchValue = searchInput.current.value;
+
+    const url = searchValue
+      ? `${window.location.pathname}?search=${searchValue}`
+      : window.location.pathname;
+
+    window.history.pushState({}, "", url);
+
     onSubmit(searchValue);
   };
+
+  const { search } = parseQuery(window.location.search);
 
   return (
     <form onSubmit={onSubmitSearch} className={search_form}>
       <input
         type="text"
         placeholder="Type your search here"
+        defaultValue={search}
         name="search"
         id="search"
         ref={searchInput}
