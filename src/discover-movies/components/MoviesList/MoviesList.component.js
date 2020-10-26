ed8183ad3df727a44bb2fa08e5ab82afbb2e5c4e
Nicolas Juarez N <nicolas.juarez@gm2dev.com>
2020-10-26 13:37:34 -0300

    feat(MoviesList.component.js): adjust aligments

--- src/discover-movies/components/MoviesList/MoviesList.component.js
+++ src/discover-movies/components/MoviesList/MoviesList.component.js
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 import styles from "./MoviesList.module.css";
 
 export function MoviesList({ movies, errorMessageResult }) {
-  return (
-    errorMessageResult || (
-      <ul className={styles.movie_list}>
-        {movies.map(({ title, id, vote_average, poster_path }) => (
-          <li key={id} className={styles.movie_list__li}>
-            <Link to={`movie/${id}`}>
-              {title} - {vote_average}
-              <img src={poster_path} alt={title} />
-            </Link>
-          </li>
-        ))}
-      </ul>
-    )
+  return errorMessageResult ? (
+    <p>{errorMessageResult}</p>
+  ) : (
+    <ul className={styles.movie_list}>
+      {movies.map(({ title, id, vote_average, poster_path }) => (
+        <li key={id} className={styles.movie_list__li}>
+          <Link to={`movie/${id}`}>
+            {title} - {vote_average}
+            <img src={poster_path} alt={title} />
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
 
